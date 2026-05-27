6d0bb5224727203b179a6da6c5c932aa38022200
Marcus DevCode <mark.tanasevich@gmail.com>
2026-05-27 20:51:06 +0300

    Update

---
+++ main.mjs
@@ -0,0 +1,122 @@
+#!/usr/bin/env node
+import { text,confirm,stream,spinner,note,select } from '@clack/prompts';
+import capitalize from "@colakit/capitalize";
+import { fileURLToPath } from 'url';
+import * as path from 'node:path'
+import {existsSync, lstatSync, mkdirSync, readdirSync, readFileSync, writeFileSync} from "node:fs";
+const cwd = process.cwd()
+const nameToSlug = (meaning,space_replacer= "") => {
+	return getSlug(meaning).replace(new RegExp("(\\s|\\t)",'g'),space_replacer)
+}
+
+function replace_data(data, replaceText) {
+	Object.keys(replaceText).map((search) => {
+		const replace = replaceText[search];
+		data = data.replace(new RegExp(search,'g'),replace);
+	})
+	return data;
+}
+
+const scan_and_replace = (__dirname, replaceText,root)  => {
+	const files_and_folders = readdirSync(__dirname);
+	const isExists          = existsSync(root);
+	console.log(isExists," isExists");
+	if(!isExists){
+		mkdirSync(root);
+	}
+	return files_and_folders.map(async (file, index) => {
+		const PathDir     = path.join(__dirname, file);
+		const lstatsync   = lstatSync(PathDir);
+		let rootFileOrDir = path.join(root, file);
+		if (file.indexOf("plugin-slug") > -1) {
+			let slug = replaceText["plugin-slug"];
+			file = file.replace(new RegExp("plugin-slug", 'g'), slug);
+			rootFileOrDir = path.join(root, file);
+		}
+		if (lstatsync.isDirectory()) {
+			console.log(PathDir," PathDir");
+			console.log(rootFileOrDir," rootFileOrDir");
+			scan_and_replace(PathDir, replaceText, rootFileOrDir);
+		}
+		if (lstatsync.isFile()) {
+			let data = readFileSync(PathDir, 'utf8');
+			data = replace_data(data, replaceText);
+			await writeFileSync(rootFileOrDir, data);
+		}
+	})
+}
+
+const nameYourPlugin = async (text) => {
+	const plugin_name = await text({
+		message: 'What is your plugin name?',
+		placeholder: ' My Awesome Plugin Name',
+		initialValue: '',
+	});
+	const projectType = await select({
+		message: 'What type of project would you like to create?',
+		options: [
+			{ value: 'page', label: 'TypeScript App Page Settings in admin', hint: 'recommended' },
+			{ value: 'blocks', label: 'TypeScript App Blocks', hint: 'recommended' },
+		],
+	});
+	let BlocksSlug = "";
+	if(projectType === "blocks"){
+		BlocksSlug = await text({
+			message: 'What is your Blocks slug?',
+			placeholder: 'my_blocks',
+			initialValue: 'my_blocks',
+		});
+	}
+	if(typeof plugin_name === "string" && typeof projectType === "string"){
+		const PluginSlug = nameToSlugCapitalize(plugin_name);
+		const PLUGIN_SLUG = nameToSlug(plugin_name,"_");
+		const PLUGINMINUSSLUG = nameToSlug(plugin_name,"-").toLowerCase();
+		const BlocksSlugLowerCase = nameToSlug(BlocksSlug,"").toLowerCase();
+		const shouldContinue = await confirm({
+			message: `Plugin folder will be ${PluginSlug}. Do you want to continue?`,
+		});
+		if(!shouldContinue){
+			return await nameYourPlugin(text);
+		}
+		const replaceText = {
+			"BlocksSlug" : BlocksSlug,
+			"blocksslug" : BlocksSlugLowerCase,
+			"Plugin_Name" : plugin_name,
+			"PluginSlug"  : PluginSlug,
+			"PLUGIN_SLUG" : PLUGIN_SLUG.toUpperCase(),
+			"plugin-slug" : PLUGINMINUSSLUG,
+		}
+		const PathDir = fileURLToPath(new URL(`./template/${projectType}`, import.meta.url))
+		const root    = path.join(cwd, PLUGINMINUSSLUG);
+		const s = spinner();
+		s.start('Creating plugin!');
+		scan_and_replace(PathDir,replaceText,root);
+		s.stop('Plugin created!');
+		stream.success((function *() { yield 'Success!'; })());
+		const nextSteps = `cd ./${PLUGINMINUSSLUG} && composer install && cd ./assets && npm install && npm start`;
+		note(nextSteps, 'Next steps.');
+		process.exit(0);
+	}
+	return false;
+}
+const getSlug = (meaning) => {
+	const regex = /\w+|\s|\t/g;
+	let meaning_name = "";
+	let m;
+	while ((m = regex.exec(meaning)) !== null) {
+		// This is necessary to avoid infinite loops with zero-width matches
+		if (m.index === regex.lastIndex) {
+			regex.lastIndex++;
+		}
+		// The result can be accessed through the `m`-variable.
+		m.forEach((match, groupIndex) => {
+			meaning_name += match;
+		});
+	}
+	return meaning_name;
+}
+const nameToSlugCapitalize = (meaning) => {
+	let meaning_name = getSlug(meaning).replace(new RegExp("(\\s|\\t)",'g'),"");
+	return capitalize(meaning_name);
+}
+nameYourPlugin(text);
